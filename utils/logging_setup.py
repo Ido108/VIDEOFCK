d574c4dcb515aaf6348f89977b9b2f6baf113b06
Ido Katzir <ido108@gmail.com>
2025-01-07 14:56:11 +0200

    updated

--- utils/logging_setup.py
+++ utils/logging_setup.py
@@ -1,14 +1,16 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+import datetime
 
 def setup_logging(app_name="videofck"):
     # Set up a logs folder in the user's home directory
     logs_folder = os.path.join(os.path.expanduser("~"), f"{app_name}_logs")
     os.makedirs(logs_folder, exist_ok=True)
+    current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
     # Define the log file path
-    log_file = os.path.join(logs_folder, "process.log")
+    log_file = os.path.join(logs_folder, f"process_{current_time}.log")
 
     try:
         # Use a rotating file handler for process.log
